50dadf8a45926989fde34dbc4a04fbbe9df99a24
ronshukrun <ronshu@post.bgu.ac.il>
2024-05-31 22:58:09 +0300

    edit class shipping and ShippingController

--- dev/src/Domain/Shipping.java
+++ dev/src/Domain/Shipping.java
@@ -3,8 +3,7 @@ package Domain;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class Shipping
-{
+public class Shipping {
     private int ShippingID;
 
     private LocalDate ShippingDate;
@@ -13,15 +12,27 @@ public class Shipping
     private Truck truck;
     ShippingStatus status;
     ShippingZone zone;
-    ShipmentInventoryManager invManager;
+    //ShipmentInventoryManager invManager;
 
 
-    public Shipping(Driver driver, Truck truck, LocalDate shippingDate, LocalTime departureTime) {
-        this.driver = driver;
-        this.truck = truck;
-        this.ShippingDate = shippingDate;
-        this.DepartureTime = departureTime;
+    public Shipping(int shippingID, ShippingZone s_zone, LocalDate ShippingDate, LocalTime DepartureTime) {
+        setShippingID(shippingID);
+        setShippingDate(ShippingDate);
+        setDepartureTime(DepartureTime);
+        setZone(s_zone);
+        status = ShippingStatus.StandBy;
     }
+
+    public Shipping(int ID, Driver driver, Truck truck, LocalDate shippingDate, LocalTime departureTime, ShippingZone shippingZone) {
+        setShippingID(ID);
+        setDriver(driver);
+        setTruck(truck);
+        setShippingDate(shippingDate);
+        setDepartureTime(departureTime);
+        setStatus(ShippingStatus.StandBy);
+        setZone(shippingZone);
+    }
+
 
     public Driver getDriver() {
         return driver;
@@ -44,8 +55,7 @@ public class Shipping
     }
 
     public void setShippingDate(LocalDate shippingDate) {
-        if (shippingDate != null)
-        {
+        if (shippingDate != null) {
             ShippingDate = shippingDate;
         }
     }
@@ -55,12 +65,61 @@ public class Shipping
     }
 
     public void setDepartureTime(LocalTime departureTime) {
-        if(departureTime!=null)
-        {
+        if (departureTime != null) {
             DepartureTime = departureTime;
         }
+    }
+
+    public int getShippingID() {
+        return ShippingID;
+    }
+
+    public void setShippingID(int shippingID) {
+        ShippingID = shippingID;
     }
     //add destination
 
 
-}
+    public ShippingStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ShippingStatus status) {
+        this.status = status;
+    }
+
+    public ShippingZone getZone() {
+        return zone;
+    }
+
+    public void setZone(ShippingZone zone) {
+        this.zone = zone;
+    }
+
+    public boolean isShippingInProgress()
+    {
+        return status == ShippingStatus.InProgress;
+    }
+    public boolean isShippingIsComplete()
+    {
+        return status == ShippingStatus.Done;
+    }
+    public boolean isShippingError()
+    {
+        return (status == ShippingStatus.ItemError || status == ShippingStatus.DriverError || status == ShippingStatus.ShipmentError);
+    }
+
+    public TypeOfLicense getLicenseForThisTruck(){
+        return truck.getTypeOfLicense();
+    }
+    public enum ShippingZone
+    {
+        North, South, East, West, Center;
+    }
+    public enum ShippingStatus
+    {
+        TruckError, DriverError, ShipmentError, ItemError, SiteError, StandBy, InProgress, Done;
+    }
+
+
+    }
--- dev/src/Domain/ShippingController.java
+++ dev/src/Domain/ShippingController.java
@@ -1,43 +1,139 @@
 package Domain;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class ShippingController {
     private static int ShippingId = 1;
     private ArrayList<Shipping> newShipments;
     private ArrayList<Shipping> errorShipments;
-
     private ArrayList<Shipping> activeShipments;
     private ArrayList<Shipping> completedShipments;
+    private static ShippingController instance;
+    private TruckController tm;
 
+    private ShippingController() {
+        newShipments = new ArrayList<>();
+        errorShipments = new ArrayList<>();
+        activeShipments = new ArrayList<>();
+        completedShipments = new ArrayList<>();
+        tm = TruckController.getInstance();
+        instance = this;
 
-    public Shipping addNewShipment() {
-        Shipping shipment = new Shipping(ShippingId);
+//        Shipping newShipping = new Shipping(ShippingId);
+//        ShippingId++;
+    }
+    public static ShippingController getInstance()
+    {
+        if (instance == null)
+            instance = new ShippingController();
+        return instance;
+    }
+
+    //A new order made by a branch manager
+    public Shipping addNewShipment(Shipping.ShippingZone shippingZone, LocalDate ShippingDate, LocalTime DepartureTime) {
+        Shipping s = new Shipping(ShippingId, shippingZone, ShippingDate,DepartureTime);
         ShippingId++;
+        newShipments.add(s);
         // add to newShipments
-        return shipment;
+        return s;
     }
 
+    //public Shipping addInfoToNewShipment(int shippingId) - this is for creating new shipping by Transport manager.
     public boolean deleteShipment(int id) {
-        Shipping shipment = getShipmentID(id);
+//        if (id<=0)
+//        {
+//            throw new IllegalArgumentException("id cannot be 0 or lower");
+//        }
+        if ((ShipmentIDExist(newShipments,id)) || ShipmentIDExist(errorShipments, id) || ShipmentIDExist(activeShipments, id) || ShipmentIDExist(completedShipments, id))
+        {
+            Shipping s = getShipmentByID(id);
+            if (newShipments.contains(s))
+            {
+                return newShipments.remove(s);
+            }
+            if (errorShipments.contains(s))
+            {
+                return errorShipments.remove(s);
+            }
+            if (activeShipments.contains(s))
+            {
+                return activeShipments.remove(s);
+            }
+        }
+        return false;
+//        throw new IllegalArgumentException("no shipment with id: "+id + " was found");
+    }
+    public boolean ShipmentIDExist(ArrayList<Shipping> arrayList, int shippingId)
+    {
+        for (Shipping shipment : arrayList)
+        {
+            if(shippingId == shipment.getShippingID())
+                return true;
+        }
+        return false;
     }
 
-    public Shipping getShipmentID(int id) {
 
+
+    public Shipping getShipmentByID(int id)
+    {
+        //if (id <= 0)
+            //throw new IllegalArgumentException("id cannot be 0 or lower");
+        for (Shipping shipment : newShipments)
+        {
+            if(id == shipment.getShippingID())
+                return shipment;
+        }
+        for (Shipping shipment : completedShipments)
+        {
+            if(id == shipment.getShippingID())
+                return shipment;
+        }
+        for (Shipping shipment : errorShipments)
+        {
+            if(id == shipment.getShippingID())
+                return shipment;
+        }
+        for (Shipping shipment : activeShipments)
+        {
+            if(id == shipment.getShippingID())
+                return shipment;
+        }
+        return null;
     }
 
-    public boolean switchStatus(Shipping shipment) {
 
+//    public boolean switchStatus(Shipping shipment)
+//    {
+//
+//    }
+    public boolean updateStatus(Shipping shipping)
+    {
+        if (shipping.status == Shipping.ShippingStatus.Done)
+        {
+            if(!completedShipments.contains(shipping))
+            {
+                return completedShipments.add(shipping) && activeShipments.remove(shipping);
+            }
+        }
+        if (shipping.status == Shipping.ShippingStatus.StandBy)
+        {
+            if (!activeShipments.contains(shipping))//and need func of cheks if the shipping is ok
+                return activeShipments.add(shipping) && newShipments.remove(shipping);
+        }
+        return false;
     }
-
     public void Managersolution() {
         // change truck
         // change site
         // change items
     }
 
-    public boolean isDriverCanDrive() {
-
+    public boolean isDriverCanDrive()
+    {
+        return false;
     }
 
     public void setShippingSource() {
@@ -45,11 +141,24 @@ public class ShippingController {
     }
 
     public boolean addShippingDestination() {
+        return false;
 
     }
     public boolean isTruckOverweight()
     {
-
+        return false;
     }
+
+    public void editShippingDate(int id, LocalDate newDate)
+    {
+        getShipmentByID(id).setShippingDate(newDate);
+    }
+
+    public void editShippingTime(int id, LocalTime newTime)
+    {
+        getShipmentByID(id).setDepartureTime(newTime);
+    }
+
+
 
 }
