4f188a16839a38319f86023bd2b8772437946447
maornezer <maorne@post.bgu.ac.il>
2024-06-02 10:40:10 +0300

    edit class driver class truck class site class TruckController

--- dev/src/Domain/TruckController.java
+++ dev/src/Domain/TruckController.java
@@ -14,16 +14,16 @@ public class TruckController
         trucks = new ArrayList<>();
         instance=this;
     }
-
     public static TruckController getInstance()
     {
         if(instance == null)
             instance = new TruckController();
         return instance;
     }
-    public boolean addTruck(String truckModel, TypeOfLicense typeOfLicense, double initialWeight, double maxWeight)
+
+    public boolean addTruck(int idTruck,double initialWeight, double maxWeight, String model)
     {
-        Truck t = new Truck(truckModel, typeOfLicense, initialWeight, maxWeight );
+        Truck t = new Truck( idTruck,  initialWeight,  maxWeight,  model);
         return addTruck(t);
     }
     public boolean addTruck(Truck t)
@@ -37,8 +37,44 @@ public class TruckController
 
     public boolean isTruckExists(Truck truck)
     {
-    return trucks.contains(truck);
+        return trucks.contains(truck);
     }
+
+    public boolean removeTruckByIDTruck(int id)
+    {
+        for (int i = 0; i<trucks.size(); i++)
+        {
+            if(trucks.get(i).getIdTruck() == id)
+            {
+                trucks.remove(i);
+                return true;
+            }
+        }
+        return false;//throw new IllegalArgumentException("Truck with this id " + id + " not found");
+    }
+    public void editTruckModelByIDTruck(int id, String model)
+    {
+        getTruckByID(id).setTruckModel(model);
+        //return false;//throw new IllegalArgumentException("Truck with this id " + id + " not found");
+    }
+
+    public void editTruckMaxWeightByIDTruck(int id, double w)
+    {
+        getTruckByID(id).setMaxWeight(w);
+        //return false;//throw new IllegalArgumentException("Truck with this id " + id + " not found");
+    }
+
+    public void editTruckInitialWeightByIDTruck(int id, double w)
+    {
+        getTruckByID(id).setInitialWeight(w);
+        //return false;//throw new IllegalArgumentException("Truck with this id " + id + " not found");
+    }
+    public void editTruckIDtByIDTruck(int oldID, int newID)
+    {
+        getTruckByID(oldID).setIdTruck(newID);
+        //return false;//throw new IllegalArgumentException("Truck with this id " + id + " not found");
+    }
+
     public Truck getTruckByLicenceType(TypeOfLicense type )
     {
         for (Truck truck : trucks)
@@ -48,20 +84,17 @@ public class TruckController
             }
         }
         return null;//throw new IllegalArgumentException("Truck with license type " + type + " not found");
-
     }
 
-    public Truck getTruckByLicencePlate(int lp )
+    public Truck getTruckByID(int id )
     {
         for (Truck truck : trucks) {
-            if (truck.getLicensePlate() == lp) {
+            if (truck.getIdTruck() == id) {
                 return truck;
             }
         }
-        return null;//throw new IllegalArgumentException("Truck with license plate " + lp + " not found");
-
+        return null;//throw new IllegalArgumentException("Truck with this id " + id + " not found");
     }
-
     private List<Truck> getAllTrucksByLicence(TypeOfLicense licenceType)
     {
         List<Truck> trucksByLicence = new ArrayList<>();
@@ -72,21 +105,27 @@ public class TruckController
         }
         return trucksByLicence;
     }
-    public boolean removeTruckByLicencePlate(int lp)
-    {
-        for (int i = 0; i<trucks.size(); i++)
-        {
-            if(trucks.get(i).getLicensePlate() == lp)
-            {
-                trucks.remove(i);
-                return true;
-            }
-        }
-        return false;
-    }
+
 
     public int getAmountOfTrucks(){return trucks.size();}
 
-
+    public String toString()
+    {
+        StringBuilder allTrucksInfo = new StringBuilder();
+        for (Truck truck : trucks) {
+            allTrucksInfo.append(truck.toString()).append("\n");
+        }
+        return allTrucksInfo.toString();
     }
+    public String printTruckByID(int id) {
+        Truck truck = getTruckByID(id);
+        if (truck != null)
+        {
+            return truck.toString();
+        }
+
+        return "Truck with ID " + id + " not found.";
+    }
+
+}
 
