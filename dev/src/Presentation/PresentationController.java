a9ae8778af306c594ccde9a86e96c528cdcaf39a
maornezer <maorne@post.bgu.ac.il>
2024-06-06 14:53:51 +0300

    edit PresentationController order func

--- dev/src/Presentation/PresentationController.java
+++ dev/src/Presentation/PresentationController.java
@@ -7,7 +7,8 @@ import Domain.TransportController;
 import java.util.ArrayList;
 import java.util.Dictionary;
 
-public class PresentationController {
+public class PresentationController
+{
 
     private DomainController domainController;
     private TransportController transportController;
@@ -21,44 +22,41 @@ public class PresentationController {
     //methods that use the controllers to make changes in the system.
     // this controller will be saved in the menu and be used from there/.
 
-    public Order createNewOrder()
-    {
-        return this.domainController.createNewOrderDomain();
-    }
 
 
-    public int creatNewOrder(Dictionary<String, String> data1, Dictionary<Integer, ArrayList<String>> data2)
-    {
-        return this.domainController.addOrder(data1, data2);
 
-    }
+    /// order  + item ///
+    public Order createNewOrder() {return this.domainController.createNewOrderDomain();}
+    public int creatNewOrder(Dictionary<String, String> data1, Dictionary<Integer, ArrayList<String>> data2)  {return this.domainController.addOrder(data1, data2);}
+    public void printAllOrders() {this.domainController.printAllOrders();}
+    public void printAllOrdersByTransport(Dictionary<String, String> data){this.transportController.printAllOrdersByTransportID(data);}
+
+    /// Transport ///
     public void addTransport(Dictionary<String, String> data)
     {
         this.transportController.addTransport(data);
     }
 
+
+    /// Site ///
     public void addSite(Dictionary<String, String> data){
         this.domainController.addSite(data);
     }
+    public void changeDestination(Dictionary<String, String> data){ this.domainController.changeDestination(data);}
+    public boolean checkAddress(String source) {return this.domainController.isAddressSiteAlreadyIn(source);}
+
+
+    /// Truck ///
     public void addTruck(Dictionary<String, String> data){
         this.domainController.addTruck(data);
     }
-
-    public void addDriver(Dictionary<String, String> data){this.domainController.addDriver(data);}
-
     public void changeTruck(Dictionary<String, String> data){this.transportController.changeTruck(data);}
 
+
+    /// Driver ///
+    public void addDriver(Dictionary<String, String> data){this.domainController.addDriver(data);}
     public void changeDriver(Dictionary<String, String> data){this.transportController.changeDriver(data);}
-    public void changeDestination(Dictionary<String, String> data){
-        this.domainController.changeDestination(data);}
-    public void printAllOrders() {this.domainController.printAllOrders();}
-   public void printAllOrdersByTransport(Dictionary<String, String> data){
-        this.transportController.printAllOrdersByTransportID(data);
-   }
-   public boolean checkAddress(String source)
-   {
-           return this.domainController.isAddressSiteAlreadyIn(source);
-   }
+
 
 
 }
